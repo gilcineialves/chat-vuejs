5e54929c4fa88e1e2beb4b13d5bcdda26cb07e4a
Gilcinei Fernandes Alves <alvesgilcinei6@gmail.com>
2024-02-13 21:54:12 -0300

    Add signup route to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,13 @@
-import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
+      name: 'signup',
+      path: '/signup',
+      component: () => import('@/views/SignUpView.vue')
+    }
   ]
 })
 
